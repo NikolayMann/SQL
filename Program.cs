1a52dd5aee1bee6f4e20ca90d93341d66c1056ba
nikolay.zolotaryov <zolotaryov_n@mail.ru>
2022-04-22 15:33:35 +0300

    no message

--- Program.cs
+++ Program.cs
@@ -1,22 +1,32 @@
 ﻿using System;
 using System.Data.SqlTypes;
 using System.Data.SqlClient;
+using System.Data.OleDb;
+using System.Data.Common;
+using System.ComponentModel.DataAnnotations;
+
 namespace SQL_Test
 {
     class Program
     {
-        static void Main(string[] args)
+        static void PingDatabase<T>(T ConnectionStringBuilder)
+            where T: DbConnectionStringBuilder
         {
-            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+            DbConnection DataBase;
+            if (ConnectionStringBuilder is SqlConnectionStringBuilder)
             {
-                DataSource = @"(localdb)\MSSQLLocalDB",
-                IntegratedSecurity = true,
-            };
-            Console.WriteLine(builder.ConnectionString);
-            using (var DB_Connection = new SqlConnection(builder.ConnectionString))
+                DataBase = new SqlConnection((ConnectionStringBuilder as T).ConnectionString);
+            }
+            else
             {
+                DataBase = new OleDbConnection((ConnectionStringBuilder as T).ConnectionString);
+            }
+
+            using (var DB_Connection = DataBase)
+            {
+                DataBase = null;
                 DB_Connection.StateChange +=
-                    (s, e) => { Console.WriteLine($"{nameof(DB_Connection)} в состоянии {(s as SqlConnection).State}"); };
+                    (s, e) => { Console.WriteLine($"{nameof(DB_Connection)} в состоянии {DB_Connection.State}"); };
                 try
                 {
                     DB_Connection.Open();
@@ -29,9 +39,42 @@ namespace SQL_Test
                 {
                     DB_Connection.Close();
                 }
-            }
+            }            
+        }
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder SQL_Builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB",
+                IntegratedSecurity = true,                
+            };
 
-
+            OleDbConnectionStringBuilder AccessBuilder = new OleDbConnectionStringBuilder()
+            {
+                Provider = @"Microsoft.ACE.OLEDB.12.0",
+                DataSource = @"C:\Users\Nikolay.DESKTOP-HRLQS2P\source\repos\SQL_Test\Database\Database5.mdb"
+            };
+            //Provider=;Data Source=C:\Users\Nikolay.DESKTOP-HRLQS2P\source\repos\SQL_Test\Database\Database5.mdb
+            Console.WriteLine(SQL_Builder.ConnectionString);
+            PingDatabase(SQL_Builder);
+            PingDatabase(AccessBuilder);
+            //using (var AccConnection = new OleDbConnection(AccessBuilder.ConnectionString))
+            //{
+            //    AccConnection.StateChange +=
+            //        (s, e) => { Console.WriteLine($"{nameof(AccConnection)} в состоянии {(s as OleDbConnection).State}"); };
+            //    try
+            //    {
+            //        AccConnection.Open();
+            //    }
+            //    catch(Exception e)
+            //    {
+            //        Console.WriteLine($"{e.Message}");
+            //    }
+            //    finally
+            //    {
+            //        AccConnection.Close();
+            //    }
+            //}
         }
     }
 }
