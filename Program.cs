a8572a9183433e737129e04e90841cb837067aa2
nikolay.zolotaryov <zolotaryov_n@mail.ru>
2022-04-22 12:25:50 +0300

    no message

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlTypes;
+using System.Data.SqlClient;
+namespace SQL_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder()
+            {
+                DataSource = @"(localdb)\MSSQLLocalDB",
+                IntegratedSecurity = true,
+            };
+            Console.WriteLine(builder.ConnectionString);
+            using (var DB_Connection = new SqlConnection(builder.ConnectionString))
+            {
+                DB_Connection.StateChange +=
+                    (s, e) => { Console.WriteLine($"{nameof(DB_Connection)} в состоянии {(s as SqlConnection).State}"); };
+                try
+                {
+                    DB_Connection.Open();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"{e.Message}");
+                }
+                finally
+                {
+                    DB_Connection.Close();
+                }
+            }
+
+
+        }
+    }
+}
